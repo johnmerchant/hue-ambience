bf1b4761c1787eba125d7bf10e2b580efb3b8a78
jmercha <john.oliver.merchant@gmail.com>
2016-01-03 03:13:55 +1030

    config

--- index.js
+++ index.js
@@ -2,27 +2,29 @@
 
 const process = require('process');
 const lossy = require('lossystream');
+const Q = require('q');
 
 const screencap = require('./lib/screen-capture');
 const bitmap = require('./lib/bitmap-stream');
 const colormap = require('./lib/color-mapper');
 const lighting = require('./lib/lighting-sink');
 
-let screen = screencap();
+function run(options) {
+   
+   let screen = screencap();
+   
+   screen.stream
+      .pipe(bitmap())
+      .pipe(colormap(options.colormap))
+      .pipe(lossy())
+      .pipe(lighting({
+         host: options.host,
+         
+      }));
+   
+   process.on('SIGINT', function () {
+      screen.end();
+      process.exit();
+   });  
+}
 
-screen.stream
-   .pipe(bitmap())
-   .pipe(colormap([{
-      light: 1,
-      x: 0,
-      y: 0,
-      w: 200,
-      h: 200
-   }]))
-   .pipe(lossy())
-   .pipe(lighting());
-
-process.on('SIGINT', function () {
-   screen.end();
-   process.exit();
-});
